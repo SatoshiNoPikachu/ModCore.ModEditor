1f0ad07f59ca818065a94206ba44b3e84319a0b5
Pikachu <1223188723@qq.com>
2026-04-22 04:20:39 +0800

    feat: log文件更名为logoutput_mc.log

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class ModEditorGUI(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         self.trans = QTranslator()
-        logInit(os.path.join(QDir.currentPath(), "logoutput.log"))
+        logInit(os.path.join(QDir.currentPath(), "logoutput_mc.log"))
         self.load_config()
         self.loadLanguage()
 
